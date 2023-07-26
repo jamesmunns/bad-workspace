a91fe070abdbdf1e6ae07ce9a87e72b6e9a51d83
James Munns <james@onevariable.com>
2023-07-26 16:41:36 +0200

    Mutually exclusive features bad

--- shared/demo-share/src/lib.rs
+++ shared/demo-share/src/lib.rs
@@ -1,5 +1,11 @@
 #![cfg_attr(not(feature = "use-std"), no_std)]
 
+#[cfg(feature = "ex-a")]
+pub const LOL: u32 = 0;
+
+#[cfg(feature = "ex-b")]
+pub const LOL: u16 = 1;
+
 pub struct One {
     pub a: u32,
     pub b: u16,
